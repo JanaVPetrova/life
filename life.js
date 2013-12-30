f93fde418d7c1d7522ac6b8fdff020bda0539ab3
Jana Petrova <janavpetrova@gmail.com>
2013-12-30 13:41:33 +0400

    add drawing

--- life.js
+++ life.js
@@ -14,7 +14,7 @@ field_next = field;
 clear_arr = field;
 
 function life(){
-  
+
   for(i=1;i < size_x;i++){
 
     for(j=1;j < size_y;j++){
@@ -27,7 +27,7 @@ function life(){
       if (field[i][j + 1] == 1) count++;
       if (field[i - 1][j] == 1) count++;
       if (field[i + 1][j] == 1) count++;
-    
+
       current = field[i][j];
       if (current == 0 && count == 3 ) {
         field_next[i][j] = 1;
@@ -42,10 +42,36 @@ function life(){
 
 function draw(){
   canvas = document.getElementById("canvas");
-  ctx= canvas.getContext('2d');
-  for (var i = 1; i < size_x; i++){
-    for (var j = 1; j < size_y; j++){
-      ctx.fillRect(0, 0, example.width, example.height);
+  ctx = canvas.getContext('2d');
+  x = 0;
+  y = 0;
+  for (var i = 1; i < size_x; i++) {
+    x = 0;
+    for (var j = 1; j < size_y; j++) {
+      if (field[j][i] == 1) {
+        ctx.fillStyle = "green"
+      } else {
+        ctx.fillStyle = "gray"
+      }
+      ctx.fillRect(x, y, 10, 10);
+      x += 10 + 1;
     }
+    y += 10 + 1;
   }
+}
+
+canvas = document.getElementById("canvas");
+canvas.addEventListener("mousedown", getPosition, false);
+
+function getPosition(event)
+{
+  var x = event.x;
+  var y = event.y;
+
+  var canvas = document.getElementById("canvas");
+
+  x -= canvas.offsetLeft;
+  y -= canvas.offsetTop;
+
+  alert("x:" + x + " y:" + y);
 }
