754c8e4bb84050b63427fd972141fdd69d2801db
plugin73 <laplugin73@gmail.com>
2013-12-30 11:54:39 +0100

    fuck

--- life.js
+++ life.js
@@ -1,23 +1,24 @@
 field = new Array();
 field_next = new Array();
-size_x = 102;
-size_y = 102;
-for(i=1;i < size_x;i++)
+size_x = 11;
+size_y = 11;
+for(i=0;i < size_x + 1;i++)
 {
   field[i] = new Array();
-  for(j=1;j < size_y; j++)
+  field_next[i] = new Array();
+  for(j=0;j < size_y + 1; j++)
   {
     field[i][j]= 0;
+    field_next[i][j]= 0;
   }
 }
-field_next = field;
 clear_arr = field;
 
 function life(){
 
-  for(i=1;i < size_x;i++){
+  for(j=1;j < size_x;j++){
 
-    for(j=1;j < size_y;j++){
+    for(i=1;i < size_y;i++){
       count = 0;
       if (field[i - 1][j - 1] == 1) count++;
       if (field[i - 1][j + 1] == 1) count++;
@@ -27,17 +28,21 @@ function life(){
       if (field[i][j + 1] == 1) count++;
       if (field[i - 1][j] == 1) count++;
       if (field[i + 1][j] == 1) count++;
-
       current = field[i][j];
       if (current == 0 && count == 3 ) {
         field_next[i][j] = 1;
       } else if (current == 1 && (count == 2 || count == 3)){
         field_next[i][j] = 1;
-      } else field_next[i][j] = 0;
+      } else if (current == 1 && (count <= 1 || count > 3)) {
+        field_next[i][j] = 0;
+      } else {
+        field_next[i][j] = 0;
+      }
     }
   }
+  tmp = field;
   field = field_next;
-  field_next = clear_arr;
+  field_next = tmp;
 }
 
 function draw(){
@@ -54,16 +59,16 @@ function draw(){
         ctx.fillStyle = "gray"
       }
       ctx.fillRect(x, y, 10, 10);
-      x += 10 + 1;
+      x += 10 ;
     }
-    y += 10 + 1;
+    y += 10 ;
   }
 }
 
 canvas = document.getElementById("canvas");
-canvas.addEventListener("mousedown", getPosition, false);
+canvas.addEventListener("mousedown", setElement, false);
 
-function getPosition(event)
+function setElement(event)
 {
   var x = event.x;
   var y = event.y;
@@ -73,5 +78,10 @@ function getPosition(event)
   x -= canvas.offsetLeft;
   y -= canvas.offsetTop;
 
-  alert("x:" + x + " y:" + y);
+  i = Math.ceil(x / 10);
+  j = Math.ceil(y / 10);
+  field[i][j] = 1;
+  draw();
 }
+
+
